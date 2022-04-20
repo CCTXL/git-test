7a13dd7cb995c8477081bcb045535a06b30fa5c7
mskk <Tmskkxl@163.com>
2022-04-20 22:28:58 +0800

    push remote repository

--- git-demo/src/main/java/com/mskk/git/Git.java
+++ git-demo/src/main/java/com/mskk/git/Git.java
@@ -7,5 +7,9 @@ public class Git {
         System.out.println("master test!");
         System.out.println("hello git3");
         System.out.println("hot-fix test");
+        System.out.println("hot-fix test");
+        System.out.println("hot-fix test");
+        System.out.println("hot-fix test");
+        System.out.println("hot-fix test");
     }
 }
